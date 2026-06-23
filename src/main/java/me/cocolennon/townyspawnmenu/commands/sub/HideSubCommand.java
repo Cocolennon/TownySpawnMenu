c923c7e8db01e6b2aa8254c940782b6439989093
Cocolennon <corentin71.lopez@gmail.com>
2026-06-23 21:44:36 +0200

    Code cleanup and MiniMessage support

--- src/main/java/me/cocolennon/townyspawnmenu/commands/sub/HideSubCommand.java
+++ src/main/java/me/cocolennon/townyspawnmenu/commands/sub/HideSubCommand.java
@@ -15,61 +15,47 @@ public class HideSubCommand {
     public static List<String> autoComplete = Arrays.asList("nation", "town");
 
     public static boolean execute(Player player, String type) {
-        Resident res = TownyAPI.getInstance().getResident(player);
-        assert res != null;
-        if(!player.hasPermission("townyspawnmenu.hide") || !player.hasPermission("townyspawnmenu.set.admin")) {
+        Resident resident = TownyAPI.getInstance().getResident(player);
+        assert resident != null;
+        if(!player.hasPermission("townyspawnmenu.hide")) {
             player.sendMessage(Localization.get(player, "error.permission", true));
             return false;
         }
         switch(type) {
             case "town" -> {
-                return hideTown(player, res);
+                if(!resident.hasTown()) {
+                    player.sendMessage(Localization.get(player, "error.towny.not-in-town", true));
+                    return false;
+                }
+                if(!resident.isMayor()) {
+                    player.sendMessage(Localization.get(player, "error.towny.not-mayor", true));
+                    return false;
+                }
+                Town town = resident.getTownOrNull();
+                Metadata.setTownHidden(town);
+                if(Metadata.getTownHidden(town)) player.sendMessage(Localization.get(player, "success.hide-town-nation", true, town.getName()));
+                else player.sendMessage(Localization.get(player, "success.show-town-nation", true, town.getName()));
+                return true;
             }
             case "nation" -> {
-                return hideNation(player, res);
+                if(!resident.hasNation()) {
+                    player.sendMessage(Localization.get(player, "error.towny.not-in-nation", true));
+                    return false;
+                }
+                if(!resident.isKing()) {
+                    player.sendMessage(Localization.get(player, "error.towny.not-king", true));
+                    return false;
+                }
+                Nation nation = resident.getNationOrNull();
+                Metadata.setNationHidden(nation);
+                if(Metadata.getNationHidden(nation)) player.sendMessage(Localization.get(player, "success.hide-town-nation", true, nation.getName()));
+                else player.sendMessage(Localization.get(player, "success.show-town-nation", true, nation.getName()));
+                return true;
             }
             default -> {
                 player.sendMessage(Localization.get(player, "error.invalid.region", true));
                 return false;
             }
         }
-    }
-
-    public static boolean hideTown(Player player, Resident res) {
-        if(!res.hasTown()) {
-            player.sendMessage(Localization.get(player, "error.towny.not-in-town", true));
-            return false;
-        }
-        if(!res.isMayor()) {
-            player.sendMessage(Localization.get(player, "error.towny.not-mayor", true));
-            return false;
-        }
-        Town town = res.getTownOrNull();
-        Metadata.setTownHidden(town);
-        if(Metadata.getTownHidden(town)) {
-            player.sendMessage(Localization.get(player, "success.hide-town-nation", true, town.getName()));
-        }else{
-            player.sendMessage(Localization.get(player, "success.show-town-nation", true, town.getName()));
-        }
-        return true;
-    }
-
-    public static boolean hideNation(Player player, Resident res) {
-        if(!res.hasNation()) {
-            player.sendMessage(Localization.get(player, "error.towny.not-in-nation", true));
-            return false;
-        }
-        if(!res.isKing()) {
-            player.sendMessage(Localization.get(player, "error.towny.not-king", true));
-            return false;
-        }
-        Nation nation = res.getNationOrNull();
-        Metadata.setNationHidden(nation);
-        if(Metadata.getNationHidden(nation)) {
-            player.sendMessage(Localization.get(player, "success.hide-town-nation", true, nation.getName()));
-        }else{
-            player.sendMessage(Localization.get(player, "success.show-town-nation", true, nation.getName()));
-        }
-        return true;
     }
 }
--- src/main/java/me/cocolennon/townyspawnmenu/commands/sub/InfoSubCommand.java
+++ src/main/java/me/cocolennon/townyspawnmenu/commands/sub/InfoSubCommand.java
@@ -3,6 +3,8 @@ package me.cocolennon.townyspawnmenu.commands.sub;
 import com.palmergames.bukkit.towny.Towny;
 import me.cocolennon.townyspawnmenu.Main;
 import me.cocolennon.townyspawnmenu.utils.Localization;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.MiniMessage;
 import org.bukkit.entity.Player;
 
 import java.util.LinkedList;
@@ -16,22 +18,23 @@ public class InfoSubCommand {
             player.sendMessage(Localization.get(player, "error.permission", true));
             return false;
         }
-        List<String> info = new LinkedList<>();
-        info.add("§c§l=========================");
-        info.add("§6§lTowny Spawn Menu " + Main.getVersion());
-        String madeFor = "0.100.4.4";
+        MiniMessage miniMessage = MiniMessage.miniMessage();
+        Main main = Main.getInstance();
+        List<Component> info = new LinkedList<>();
+        info.add(miniMessage.deserialize("<#FF5555><bold>========================="));
+        info.add(miniMessage.deserialize("<#FFAA00><bold>Towny Spawn Menu " + main.getVersion()));
+        String madeFor = "0.103.0.0";
         if(!townyVersion.equals(madeFor)){
-            info.add("§6Made for §lTowny " + madeFor + " §6(using §lTowny v" + townyVersion + "§6)");
+            info.add(miniMessage.deserialize("<#FFAA00>Made for <bold>Towny " + madeFor + " </bold>(using <bold>Towny v" + townyVersion + "</bold>)"));
         }else {
-            info.add("§6Made for §lTowny " + madeFor);
+            info.add(miniMessage.deserialize("<#FFAA00>Made for <bold>Towny " + madeFor));
         }
-        if(Main.getUsingOldVersion()){
-            info.add("§6An update is available!");
+        if(main.getUsingOldVersion()){
+            info.add(miniMessage.deserialize("<#FFAA00>An update is available!"));
         }else{
-            info.add("§6You're using the latest version");
+            info.add(miniMessage.deserialize("<#FFAA00>You're using the latest version"));
         }
-        info.add("§c§l=========================");
-
+        info.add(miniMessage.deserialize("<#FF5555><bold>========================="));
         info.forEach(player::sendMessage);
         return true;
     }
